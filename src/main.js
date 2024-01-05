97f2a3eaf745ac404be8c8e79de84e43fdfbbe3d
Igor Dantas <igordantasdeholanda@hotmail.com>
2024-01-05 16:14:42 -0300

    Primeiro Commit ( Subindo Portfólio Para o GitHub )

---
+++ src/main.js
@@ -0,0 +1,32 @@
+// função para esconder e ixibir o menu na versão mobile
+const btn = document.getElementById("Botao")
+        const menu = document.getElementById("menu")
+        btn.addEventListener("click" , () => {
+            menu.classList.toggle("hidden")
+        })
+
+        // Máquina de escrever na página HOME
+        function typeWrite(elemento){
+            const textoArray = elemento.innerHTML.split('');
+            elemento.innerHTML = '';
+            textoArray.forEach((letra,i) => {
+                setTimeout(() => elemento.innerHTML += letra, 75 * i)
+            });
+        }
+        const titulo = document.querySelector('h2');
+        typeWrite(titulo);
+        
+        const textLoad = () => {
+            setTimeout(() => {
+                titulo.tituloContent = 'Web Developer';
+            }, 0);
+            setTimeout (() => {
+                titulo.tituloContent = 'Desenvolvedor Web';
+            }, 4000);
+            setTimeout (() => {
+                titulo.tituloContent = 'Desenvolvedor face';
+            },8000);
+        }
+            textLoad();
+            setInterval(textLoad,12000);
+    
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      spacing:{
+        '128': '32px',
+        '144': '36px',
+        
+      }
+    },
+  },
+  plugins: [],
+}
+
