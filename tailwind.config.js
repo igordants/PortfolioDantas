caa35f221cbb5d3f0c15badf2030f6fc63f1305f
Igor Dantas <113206684+igordants@users.noreply.github.com>
2024-01-13 22:32:10 -0300

    Configurando arquivo tailwind.config.js ( text-Shadow )

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,26 @@ module.exports = {
         '128': '32px',
         '144': '36px',
     },
+    textShadow: {
+      sm: '0 1px 2px var(--tw-shadow-color)',
+      DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+      lg: '0 8px 16px var(--tw-shadow-color)',
+    },
+  },
     inset:{
       '17':'68px'
     }
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') }
+      )
+    }),
+  ],
   }
-}
